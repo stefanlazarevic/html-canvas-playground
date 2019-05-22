1a9e9d888e60fcc057d3f24b6697a91c5578b12b
Stefan Lazarevic <stefanlazarevic.contact@gmail.com>
2019-05-22 23:44:03 +0200

    Playing with the movement and animation.

--- html-canvas-playground/canvas.js
+++ html-canvas-playground/canvas.js
@@ -65,20 +65,78 @@ function createLine(context, startXPos, startYPos, endXPos, endYPos, color = "#0
     }
 }
 
-function drawCircle(context, xPos, yPos, radius, color = "#000", anticlockwise = false) {
+/**
+ * Creates and draws empty arc/curve shape on the given canvas drawing.
+ *
+ * @param {CanvasRenderingContext2D} context
+ * @param {number} xPos
+ * @param {number} yPos
+ * @param {number} radius
+ * @param {number} startAngle
+ * @param {number} endAngle
+ * @param {string} color
+ * @param {boolean} anticlockwise
+ */
+function drawArc(context, xPos, yPos, radius, startAngle, endAngle, color = "#000", anticlockwise = false) {
     context.beginPath();
     context.strokeStyle = color;
-    context.arc(xPos, yPos, radius, 0, Math.PI * 2, anticlockwise);
+    context.arc(xPos, yPos, radius, startAngle, endAngle, anticlockwise);
     context.stroke();
 }
 
-function drawFilledCircle(context, xPos, yPos, radius, color = "#000", anticlockwise = false) {
+/**
+ * Creates and draws filled arc/curve shape on the given canvas drawing.
+ *
+ * @param {CanvasRenderingContext2D} context
+ * @param {number} xPos
+ * @param {number} yPos
+ * @param {number} radius
+ * @param {number} startAngle
+ * @param {number} endAngle
+ * @param {string} color
+ * @param {boolean} anticlockwise
+ */
+function drawFilledArc(context, xPos, yPos, radius, startAngle, endAngle, color = "#000", anticlockwise = false) {
     context.beginPath();
     context.fillStyle = color;
-    context.arc(xPos, yPos, radius, 0, Math.PI * 2, anticlockwise);
+    context.arc(xPos, yPos, radius, startAngle, endAngle, anticlockwise);
     context.fill();
 }
 
+/**
+ * Creates and draws empty circle shape on the given canvas drawing.
+ *
+ * @param {CanvasRenderingContext2D} context
+ * @param {number} xPos
+ * @param {number} yPos
+ * @param {number} radius
+ * @param {string} color
+ * @param {boolean} anticlockwise
+ */
+function drawCircle(context, xPos, yPos, radius, color = "#000", anticlockwise = false) {
+    this.drawArc(context, xPos, yPos, radius, 0, Math.PI * 2, color, anticlockwise);
+}
+
+/**
+ * Creates and draws filled circle shape on the given canvas drawing.
+ *
+ * @param {CanvasRenderingContext2D} context
+ * @param {number} xPos
+ * @param {number} yPos
+ * @param {number} radius
+ * @param {string} color
+ * @param {boolean} anticlockwise
+ */
+function drawFilledCircle(context, xPos, yPos, radius, color = "#000", anticlockwise = false) {
+    this.drawFilledArc(context, xPos, yPos, radius, 0, Math.PI * 2, color, anticlockwise);
+}
+
+/**
+ * Clears given canvas for redrawing.
+ *
+ * @param {HTMLCanvasElement} canvas
+ * @param {CanvasRenderingContext2D} context
+ */
 function clearCanvas(canvas, context) {
-    context.clearCanvas(0, 0, canvas.width, canvas.height);
+    context.clearRect(0, 0, canvas.width, canvas.height);
 }
--- html-canvas-playground/index.js
+++ html-canvas-playground/index.js
@@ -5,11 +5,37 @@
 const canvas = document.getElementById('canvas');
 
 /** Set canvas width and height to fill the window size. */
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+function recalculateCanvasDimensions(canvas) {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+}
+
+window.onresize = () => {
+    recalculateCanvasDimensions(canvas);
+};
+
+recalculateCanvasDimensions(canvas);
 
 const context = canvas.getContext('2d');
 
+let x = 200;
+
+let VELOCITY = 4;
+const RADIUS = 30;
+
 function animate() {
     requestAnimationFrame(animate);
+
+    clearCanvas(canvas, context);
+
+    /** @memberof ./canvas.js */
+    drawFilledCircle(context, x, 100, RADIUS);
+
+    if (x + RADIUS > window.innerWidth || x - RADIUS < 0) {
+        VELOCITY = -VELOCITY;
+    }
+
+    x += VELOCITY;
 }
+
+animate();

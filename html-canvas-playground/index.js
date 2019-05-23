63ef486626bf095b2668e335c4a721526fc8c235
Stefan Lazarevic <stefanlazarevic.contact@gmail.com>
2019-05-23 23:40:11 +0200

    Added ball animation and color changing.

--- html-canvas-playground/index.js
+++ html-canvas-playground/index.js
@@ -3,25 +3,25 @@
  * @public
  */
 const canvas = document.getElementById('canvas');
+const context = canvas.getContext('2d');
+
+const BALL_RADIUS = 30;
+const BALL_SPEED = 4;
 
 /** Set canvas width and height to fill the window size. */
 function recalculateCanvasDimensions(canvas) {
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
+    canvas.width = document.body.clientWidth;
+    canvas.height = document.body.clientHeight;
 }
 
-window.onresize = () => {
-    recalculateCanvasDimensions(canvas);
-};
-
+window.onresize = () => recalculateCanvasDimensions(canvas);
 recalculateCanvasDimensions(canvas);
 
-const context = canvas.getContext('2d');
 
-let x = 200;
-
-let VELOCITY = 4;
-const RADIUS = 30;
+let x = Math.random() * (canvas.width - BALL_RADIUS * 2) + BALL_RADIUS;
+let y = Math.random() * (canvas.height - BALL_RADIUS * 2) + BALL_RADIUS;
+let dx = BALL_SPEED;
+let dy = BALL_SPEED;
 
 function animate() {
     requestAnimationFrame(animate);
@@ -29,13 +29,31 @@ function animate() {
     clearCanvas(canvas, context);
 
     /** @memberof ./canvas.js */
-    drawFilledCircle(context, x, 100, RADIUS);
+    drawFilledCircle(context, x, y, BALL_RADIUS, randomColor());
 
-    if (x + RADIUS > window.innerWidth || x - RADIUS < 0) {
-        VELOCITY = -VELOCITY;
+    if (x + BALL_RADIUS > canvas.width || x - BALL_RADIUS < 0) {
+        dx = -dx;
     }
 
-    x += VELOCITY;
+    if (y + BALL_RADIUS > canvas.height || y - BALL_RADIUS < 0) {
+        dy = -dy;
+    }
+
+    x += dx;
+    y += dy;
 }
 
 animate();
+
+function randomColor() {
+    const chars ="0123456789abcdef";
+    const prefix = "#";
+    let color = prefix;
+
+    for (let i = 0; i < 6; i++) {
+      const index = Math.floor(16 * Math.random());
+      color += chars.charAt(index);
+    }
+
+    return color;
+  }
